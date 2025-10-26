13b4809722c26325523ff713cc7689770a081a8a
millan-figueroa <millan.fig@gmail.com>
2025-10-26 14:52:33 -0700

    Creates function to fetch products from API and throws APIError

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,8 +1,18 @@
-import Product from "../models/Product";
-import { APIError, ValidationError } from "../utils/errorHandler";
+
+import { APIError, handleAPIError } from "../utils/errorHandler";
 
 const BASE_URL = "https://dummyjson.com/products";
 
-export async function fetchProducts() {
-    console.log('Testing...')
+export async function fetchProducts(limit: number = 10) {
+    try {
+        const response = await fetch(`${BASE_URL}/products?limit=${limit}`);
+        if (!response.ok) {
+            throw new APIError(`Error fetching products.`, response.status);
+        }
+
+        const data = await response.json();
+        return data.products;
+    } catch (error: APIError | any) {
+        handleAPIError(error);
+    }
 }
